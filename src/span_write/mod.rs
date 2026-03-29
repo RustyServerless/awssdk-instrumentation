7ad5309944a7adbbc1230f429fed017da1252203
Jérémie Rodon <jeremie.rodon@gmail.com>
2026-03-29 20:20:02 +0200

    wi

--- src/span_write/mod.rs
+++ src/span_write/mod.rs
@@ -5,9 +5,13 @@ mod tracing;
 mod otel;
 
 pub use opentelemetry::{Value, trace::Status};
+use opentelemetry_semantic_conventions::trace::HTTP_RESPONSE_STATUS_CODE;
 
 // Backend-agnostic interface for injecting attributes and status into a span.
 pub trait SpanWrite {
     fn set_attribute(&mut self, key: &'static str, value: impl Into<Value>);
     fn set_status(&mut self, code: Status);
+    fn set_http_status_code(&mut self, code: u16) {
+        self.set_attribute(HTTP_RESPONSE_STATUS_CODE, code as i64);
+    }
 }
